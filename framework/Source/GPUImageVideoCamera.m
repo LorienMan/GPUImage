d52744d171d7731096ef2ab5db5bdfed1e34c2cc
Ilya Amelchenkov <ilya.amelchenkov@gmail.com>
2014-02-26 13:33:08 +0400

    fix force focus

--- framework/Source/GPUImageVideoCamera.m
+++ framework/Source/GPUImageVideoCamera.m
@@ -541,6 +541,16 @@ NSString *const kGPUImageYUVVideoRangeConversionForLAFragmentShaderString = SHAD
         _inputCamera.focusMode = AVCaptureFocusModeAutoFocus;
         [_inputCamera unlockForConfiguration];
     }
+    
+    AVCaptureDevice *localDevice = _inputCamera;
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        if ([localDevice isFocusModeSupported:AVCaptureFocusModeContinuousAutoFocus] && [localDevice lockForConfiguration:NULL]) {
+            localDevice.focusMode = AVCaptureFocusModeContinuousAutoFocus;
+            [localDevice unlockForConfiguration];
+        }
+    });
 }
 
 - (void)setCaptureSessionPreset:(NSString *)captureSessionPreset;
